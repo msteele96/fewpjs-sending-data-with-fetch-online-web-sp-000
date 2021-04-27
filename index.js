1aebb772b88c9ad92ae054b42698815deb4de1f4
Marc Steele <marcsteele2014@gmail.com>
2021-04-27 19:39:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData( name, email ){
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( {
+      name,
+      email
+    } )
+  }
+    return fetch('http://localhost:3000/users', configObj)
+    .then( function ( resp ) {
+      return resp.json()
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"]
+    } )
+    .catch( function ( error ) {
+      document.body.innerHTML = error.message
+    } )
+};
