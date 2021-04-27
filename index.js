cbdceed4ec38ea784334e2ff6c41a4b5d4bebe67
Marc Steele <marcsteele2014@gmail.com>
2021-04-27 19:40:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Add your code here
 function submitData( name, email ){
+<<<<<<< HEAD
   const configObj = {
     method: "POST",
     headers: {
@@ -22,3 +23,59 @@ function submitData( name, email ){
       document.body.innerHTML = error.message
     } )
 };
+=======
+    return fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    })
+    .then( function ( resp ) {
+      return resp.json()
+    })
+    // .then(function(object) {
+    //   document.body.innerHTML = object["id"]
+    // } )
+};
+
+const configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify( {
+    name,
+    email
+  } )
+}
+
+//
+// function submitData(name, email) {
+//   return fetch( 'http://localhost:3000/users', {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify({
+//         name,
+//         email
+//       })
+//     })
+//     .then(function(response){
+//       return response.json()
+//     })
+// //     .then( function ( object ) {
+// //       document.body.innerHTML = object[ "id" ]
+// //     } )
+// //     .catch( function ( error ) {
+// //       document.body.innerHTML = error.message
+// //     } )
+// }
+>>>>>>> da8e0cd87dd25f65c8f36d0e896117a898384e12
